f53fa2e7abad12ef286b3d6518e0f99483fc01a7
iamsweelai <iamsweelai@gmail.com>
2017-12-21 16:43:06 +0800

    Update library Add Hansap flavor

--- app/src/main/java/sg/lifecare/medicare/VitalConfigs.java
+++ app/src/main/java/sg/lifecare/medicare/VitalConfigs.java
@@ -2,5 +2,5 @@ package sg.lifecare.medicare;
 
 public class VitalConfigs {
 
-    public static final String URL = "http://vitals.hansap.com/";
+    public static final String URL = BuildConfig.SERVER_URL;
 }
